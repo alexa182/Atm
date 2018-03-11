57b1e69bb80cf9d593c61a5593854854ec95fecd
alexa182 <33422462+alexa182@users.noreply.github.com>
2018-03-11 13:45:22 +0200

    Update logariasmos.c

--- logariasmos.c
+++ logariasmos.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
-float arxiko=1000;
-int xronos;
-
 int main()
-{
-	for(xronos=1;xronos<30;xronos++)
+{	
+	float arxiko=1000;
+	int xronos;	
+
+	while(arxiko<=2000)
 	{
+		xronos++;
+
 		arxiko=arxiko+(arxiko*0.03);
 		printf("Xronia: %d Poso: %.2f$\n",xronos,arxiko);				
 		if(arxiko>=2000)	
 			break;
-	}	
+	}
 }
