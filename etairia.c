a5cad9dd28231712fb055c1ffc1620aead2d3d1b
alexa182 <33422462+alexa182@users.noreply.github.com>
2018-03-11 18:12:30 +0200

    Update etairia.c

--- etairia.c
+++ etairia.c
@@ -31,11 +31,11 @@ int main()
 			sunolo=sunolo-1;
 		}
 		if(apantisi_1>apantisi_2 && apantisi_1>apantisi_3)
-			printf("\nPerisoteres apantiseis #1 : %2.f%%\n",(float)(apantisi_1/sunolo)*100);
+			printf("\nPerisoteres apantiseis #1 : %.2f%%\n",(float)(apantisi_1/sunolo)*100);
 		else if(apantisi_2>apantisi_1 && apantisi_2>apantisi_3)
-			printf("\nPerisoteres apantiseis #2 : %2.f%%\n",(float)(apantisi_2/sunolo)*100);
+			printf("\nPerisoteres apantiseis #2 : %.2f%%\n",(float)(apantisi_2/sunolo)*100);
 		else if(apantisi_3>apantisi_1 && apantisi_3>apantisi_2)
-			printf("\nPerisoteres apantiseis #3 : %2.f%%\n",(float)(apantisi_3/sunolo)*100);
+			printf("\nPerisoteres apantiseis #3 : %.2f%%\n",(float)(apantisi_3/sunolo)*100);
 	}
 
 }
