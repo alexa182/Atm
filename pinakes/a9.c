8704ba77c28d93d63d95ed8cf3dacdecb1981743
alexa182 <33422462+alexa182@users.noreply.github.com>
2018-04-03 15:54:52 +0300

    Update a9.c

--- pinakes/a9.c
+++ pinakes/a9.c
@@ -2,15 +2,15 @@
 
 int main()
 {
-	int pinakas1[]={1,2,3,4,5,6,7,8,9,10},pinakas2[]={10,20,30,40,50,60,70,80,90,100},swap[10];
+	int pinakas1[]={1,2,3,4,5,6,7,8,9,10},pinakas2[]={10,20,30,40,50,60,70,80,90,100},swap;
 
 	for(int x=0;x<10;x++)
 	{
-		swap[x]=pinakas2[x];
+		swap=pinakas2[x];
 		pinakas2[x]=pinakas1[x];
-		pinakas1[x]=swap[x];
+		pinakas1[x]=swap;
 	}
-	for(int y=0;y<10;y++)
-		printf("pinakas#1 : %d pinakas#2 : %d\n",pinakas1[y],pinakas2[y]);
+	for(int x=0;x<10;x++)
+		printf("pinakas#1 : %d pinakas#2 : %d\n",pinakas1[x],pinakas2[x]);
 
 }
